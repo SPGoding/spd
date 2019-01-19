35f4f37ea89483e48ed64a31c46117fe3d06c5f8
SPGoding <SPGoding@outlook.com>
2019-01-19 19:39:20 +0800

    :sparkles:Ready to write server.

--- server/out/server.js
+++ server/out/server.js
@@ -1,148 +1,27 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode_languageserver_1 = require("vscode-languageserver");
-let connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
-let documents = new vscode_languageserver_1.TextDocuments();
-let hasConfigurationCapability = false;
-let hasWorkspaceFolderCapability = false;
-let hasDiagnosticRelatedInformationCapability = false;
-connection.onInitialize((params) => {
-    let capabilities = params.capabilities;
-    hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
-    hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
-    hasDiagnosticRelatedInformationCapability =
-        !!(capabilities.textDocument &&
-            capabilities.textDocument.publishDiagnostics &&
-            capabilities.textDocument.publishDiagnostics.relatedInformation);
+const connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
+connection.onInitialize(() => {
     return {
         capabilities: {
-            textDocumentSync: documents.syncKind,
-            completionProvider: {
+            completionProvider: {},
+            definitionProvider: true,
+            documentHighlightProvider: true,
+            hoverProvider: true,
+            referencesProvider: true,
+            renameProvider: true,
+            documentLinkProvider: {
                 resolveProvider: true
+            },
+            signatureHelpProvider: {
+                triggerCharacters: [' ']
+            },
+            textDocumentSync: {
+                change: vscode_languageserver_1.TextDocumentSyncKind.Incremental,
+                openClose: true
             }
         }
     };
 });
-connection.onInitialized(() => {
-    if (hasConfigurationCapability) {
-        connection.client.register(vscode_languageserver_1.DidChangeConfigurationNotification.type, undefined);
-    }
-    if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders(_event => {
-            connection.console.log('Workspace folder change event received.');
-        });
-    }
-});
-const defaultSettings = { maxNumberOfProblems: 1000 };
-let globalSettings = defaultSettings;
-let documentSettings = new Map();
-connection.onDidChangeConfiguration(change => {
-    if (hasConfigurationCapability) {
-        documentSettings.clear();
-    }
-    else {
-        globalSettings = ((change.settings.languageServerExample || defaultSettings));
-    }
-    documents.all().forEach(validateTextDocument);
-});
-function getDocumentSettings(resource) {
-    if (!hasConfigurationCapability) {
-        return Promise.resolve(globalSettings);
-    }
-    let result = documentSettings.get(resource);
-    if (!result) {
-        result = connection.workspace.getConfiguration({
-            scopeUri: resource,
-            section: 'languageServerExample'
-        });
-        documentSettings.set(resource, result);
-    }
-    return result;
-}
-documents.onDidClose(e => {
-    documentSettings.delete(e.document.uri);
-});
-documents.onDidChangeContent(change => {
-    validateTextDocument(change.document);
-});
-function validateTextDocument(textDocument) {
-    return __awaiter(this, void 0, void 0, function* () {
-        let settings = yield getDocumentSettings(textDocument.uri);
-        let text = textDocument.getText();
-        let pattern = /\b[A-Z]{2,}\b/g;
-        let m;
-        let problems = 0;
-        let diagnostics = [];
-        while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-            problems++;
-            let diagnosic = {
-                severity: vscode_languageserver_1.DiagnosticSeverity.Warning,
-                range: {
-                    start: textDocument.positionAt(m.index),
-                    end: textDocument.positionAt(m.index + m[0].length)
-                },
-                message: `${m[0]} is all uppercase.`,
-                source: 'ex'
-            };
-            if (hasDiagnosticRelatedInformationCapability) {
-                diagnosic.relatedInformation = [
-                    {
-                        location: {
-                            uri: textDocument.uri,
-                            range: Object.assign({}, diagnosic.range)
-                        },
-                        message: 'Spelling matters'
-                    },
-                    {
-                        location: {
-                            uri: textDocument.uri,
-                            range: Object.assign({}, diagnosic.range)
-                        },
-                        message: 'Particularly for names'
-                    }
-                ];
-            }
-            diagnostics.push(diagnosic);
-        }
-        connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-    });
-}
-connection.onDidChangeWatchedFiles(_change => {
-    connection.console.log('We received an file change event');
-});
-connection.onCompletion((_textDocumentPosition) => {
-    return [
-        {
-            label: 'TypeScript',
-            kind: vscode_languageserver_1.CompletionItemKind.Text,
-            data: 1
-        },
-        {
-            label: 'JavaScript',
-            kind: vscode_languageserver_1.CompletionItemKind.Text,
-            data: 2
-        }
-    ];
-});
-connection.onCompletionResolve((item) => {
-    if (item.data === 1) {
-        (item.detail = 'TypeScript details'),
-            (item.documentation = 'TypeScript documentation');
-    }
-    else if (item.data === 2) {
-        (item.detail = 'JavaScript details'),
-            (item.documentation = 'JavaScript documentation');
-    }
-    return item;
-});
-documents.listen(connection);
-connection.listen();
 //# sourceMappingURL=server.js.map
